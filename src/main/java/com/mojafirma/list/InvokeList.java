ba682adbfbc4ef511bd27b15261bca6eaaf7a64b
dareczkaloreczka <dk.biernat@gmail.com>
2017-08-26 09:35:07 +0200

    +list removal ammended

--- src/main/java/com/mojafirma/list/InvokeList.java
+++ src/main/java/com/mojafirma/list/InvokeList.java
@@ -7,8 +7,12 @@ public class InvokeList {
         List list = new List();
         list.addToList(10);
         list.addToList(11);
+        list.addToList(12);
+        list.addToList(13);
+        list.addToList(14);
         list.addToList(5);
-        list.removeByValue(11);
+        list.removeByValue(10);
         list.printList();
+        list.revPrintList();
     }
 }
--- src/main/java/com/mojafirma/list/List.java
+++ src/main/java/com/mojafirma/list/List.java
@@ -5,31 +5,31 @@ public class List {
     private ListElement first = null;
     private ListElement last = null;
 
-    public void addToList(int value){
+    public void addToList(int value) {
         ListElement item = new ListElement(value);
-        if (first == null){
+        if (first == null) {
             first = item;
         }
-        if (last != null){
+        if (last != null) {
             last.setNext(item);
             item.setPrev(last);
         }
         last = item;
     }
 
-    public void removeFirst(){
-        if(first != null){
+    public void removeFirst() {
+        if (first != null) {
             first = first.getNext();
         }
     }
-    public void removeLast(){
-        if(last != null){
+
+    public void removeLast() {
+        if (last != null) {
             last = last.getPrev();
         }
     }
 
-    public void removeByValue(int value){
-
+    public void removeByValue(int value) {
 
         ListElement temp = first;
         if (temp != null) {
@@ -38,18 +38,33 @@ public class List {
             }
             if (temp.getPrev() != null) {
                 temp.getPrev().setNext(temp.getNext());
+            } else {
+                first = temp.getNext();
+                first.setPrev(null);
             }
             if (temp.getNext() != null) {
-                temp.getNext().setPrev(temp.getPrev());
+                if (temp.getPrev() != null) {
+                    temp.getNext().setPrev(temp.getPrev());
+                }
             }
         }
     }
-    public void printList(){
+
+    public void printList() {
         ListElement temp = first;
-        while (temp != null){
+        while (temp != null) {
             System.out.println(temp.getValue());
             temp = temp.getNext();
         }
     }
+    public void revPrintList() {
+        ListElement temp = last;
+        while (temp != null) {
+            System.out.println(temp.getValue());
+            temp = temp.getPrev();
+        }
+    }
+
+
 
 }
